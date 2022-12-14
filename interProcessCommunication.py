6c11fd365d644cf2dac8413cbfe0972b64e060f7
kctaids <120014444+kctaids@users.noreply.github.com>
2022-12-14 13:10:22 +0530

    Create interProcessCommunication.py

---
+++ interProcessCommunication.py
@@ -0,0 +1,27 @@
+from multiprocessing import Process, Pipe
+
+def f(conn):
+    conn.send([42, None, 'hello'])
+    conn.close()
+
+if __name__ == '__main__':
+    parent_conn, child_conn = Pipe()
+    p = Process(target=f, args=(child_conn,))
+    p.start()
+    print(parent_conn.recv())   # prints "[42, None, 'hello']"
+    p.join()
+    
+    from multiprocessing import Process, Lock
+
+def f(l, i):
+    l.acquire()
+    try:
+        print('hello world', i)
+    finally:
+        l.release()
+
+if __name__ == '__main__':
+    lock = Lock()
+
+    for num in range(10):
+        Process(target=f, args=(lock, num)).start()
