85250bacac6f3fdb9d33be07dbec8ae75a72b937
sunday-69 <120014444+sunday-69@users.noreply.github.com>
2023-01-09 09:31:14 +0530

    Update sharedMemory.py

--- sharedMemory.py
+++ sharedMemory.py
@@ -3,12 +3,12 @@ import multiprocessing
 def square_list(mylist, result, square_sum):
     for idx, num in enumerate(mylist): 
         result[idx] = num * num
-    print("Result(in process p1): {}".format(result[:]))
-    print("Sum of squares(in process p1): {}".format(square_sum.value))
+    print(f"Result(in process p1): {result[:]}")
+    print(f"Sum of squares(in process p1): {square_sum.value}")
 
 if   name   == "__main ": 
     mylist = [1,2,3,4]
     result = multiprocessing.Array('i', 4)
     p1 = multiprocessing.Process(target=square_list, args=(mylist, result, square_sum))
-    print("Result(in main program): {}".format(result[:]))
-    print("Sum of squares(in main program): {}".format(square_sum.value))
+    print(f"Result(in main program): {format(result[:]}")
+    print(f"Sum of squares(in main program): {(square_sum.value}")
